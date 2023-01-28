84b0d820fda7973734fe33720cbde63f0b4c9753
Chueyyew <hueyyew.cheong@gmail.com>
2023-01-28 21:29:40 +0000

    moved plot function

--- functions/linearise.py
+++ functions/linearise.py
@@ -26,3 +26,22 @@ def propogate_error(x,xerr,y,yerr):
         ys.append(yi)
         ycount=ycount+1
     return xs,ys
+
+def plot(x,y,xerr,yerr):
+    fig,ax=plt.subplots()
+    a, b = np.polyfit(x, y, 1)
+    
+    ypoints=[]
+    for i in x:
+        ypoints.append(a*i+b)
+    
+    ax.plot(x, ypoints,color='black')
+
+    ax.errorbar(x, y, yerr=yerr,xerr=xerr, fmt='none',ecolor='#000000',color='#000000',elinewidth=0.5,capsize=2)
+    
+    ax.text(-1, 0.35, 'y = ' + '{:.5f}'.format(a) + 'x' + ' + {:.5f}'.format(b), size=10)
+    ax.set_xlabel("Distance R, r (m)")
+    ax.set_ylabel('Amplitude of Wave, A (V)')
+    ax.tick_params(axis='both',direction='inout')
+    
+    plt.savefig('Graph1',dpi=500)
