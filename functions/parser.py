94b4376e7a884d6db44bea386739307ec8180b9c
Chueyyew <hueyyew.cheong@gmail.com>
2023-01-28 20:51:59 +0000

    Modularising

--- functions/parser.py
+++ functions/parser.py
@@ -16,4 +16,93 @@ def readfile(source):
     
     return ndict
 
+def points():
+    a = int(input('Number of Points: ')) #Establishing the number of data points overall
+    b = int(input('Number of X readings: ')) #Entering the various X values, factoring multiple readings
+    xs,ys,xerrs,yerrs=[],[],[],[]
+    
+    constant_x_error=input('Is X Error Constant (Y/N)')
+    constant_y_error=input('Is Y Error Constant (Y/N)')
+    
+    if constant_y_error=='Y' or 'y':
+        yerr=float(input('Error on Y:'))
+        
+        if constant_x_error=='Y' or 'y':
+            xerr=float(input('Error on X:'))
+        
+            for i in range(a):
+                y=float(input('Y: ')) #Entering the various Y values, whilst iterating across the number of data points
 
+                if b>1:
+                    xls=[]
+                    for i in range(b):  
+                        xls.append(xerr)
+                    x,xerr=stats(xls) #Calculating error
+                elif b==1:
+                    x=float(input('X: '))
+
+                print("Point is ("+str(x)+","+str(y)+")")
+                xs.append(x)
+                ys.append(y)
+                xerrs.append(xerr)
+                yerrs.append(yerr)
+        else:
+            for i in range(a):
+                y=float(input('Y: ')) #Entering the various Y values, whilst iterating across the number of data points
+
+                if b>1:
+                    xls=[]
+                    for i in range(b):  
+                        xls.append(float(input('X: ')))
+                    x,xerr=stats(xls) #Calculating error
+                elif b==1:
+                    x=float(input('X: '))
+
+                print("Point is ("+str(x)+","+str(y)+")")
+                xs.append(x)
+                ys.append(y)
+                xerrs.append(xerr)
+                yerrs.append(yerr)
+    else:
+        if constant_x_error=='Y' or 'y':
+            xerr=float(input('Error on Y:'))
+        
+            for i in range(a):
+                y=float(input('Y: ')) #Entering the various Y values, whilst iterating across the number of data points
+                yerr=float(input('Error on Y:'))
+                
+                if b>1:
+                    xls=[]
+                    for i in range(b):  
+                        xls.append(xerr)
+                    x,xerr=stats(xls) #Calculating error
+                elif b==1:
+                    x=float(input('X: '))
+                    xerr=float(input('X Error: '))
+
+                print("Point is ("+str(x)+","+str(y)+")")
+                xs.append(x)
+                ys.append(y)
+                xerrs.append(xerr)
+                yerrs.append(yerr)
+        else:
+            for i in range(a):
+                y=float(input('Y: ')) #Entering the various Y values, whilst iterating across the number of data points
+                yerr=float(input('Error on Y:'))
+                
+                if b>1:
+                    xls=[]
+                    for i in range(b):  
+                        xls.append(float(input('X: ')))
+                    x,xerr=stats(xls) #Calculating error
+                elif b==1:
+                    x=float(input('X: '))
+                    xerr=float(input('X Error: '))
+
+                print("Point is ("+str(x)+","+str(y)+")")
+                xs.append(x)
+                ys.append(y)
+                xerrs.append(xerr)
+                yerrs.append(yerr)
+
+    return xs,ys,xerrs,yerrs
